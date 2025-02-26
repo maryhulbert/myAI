b55a02c361a2f7c8892b7b66029bc20a9f00275d
maryhulbert <maryhulbert22@gmail.com>
2025-02-26 11:55:41 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,9 +1,13 @@
-export const CHAT_HEADER = "Hello, I'm [CaseyTheCareerBot], [Mary Hulbert]’s AI assistant.";
+export const CHAT_HEADER = "Hello, I'm CaseyTheCareerBot, Mary Hulbert’s AI assistant.";
 
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[Helping MBA Students land dream jobs]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${CaseyTheCareerBot}, ${Mary Hulbert's}'s AI assistant.`;
+export const FOOTER_MESSAGE = "Helping MBA students land dream jobs.";
 
-export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
+export const MESSAGE_PLACEHOLDER = "[MESSAGE PLACEHOLDER]";
+
+export const CLEAR_BUTTON_TEXT = "[CLEAR BUTTON TEXT]";
+
+export const PAGE_TITLE = "[PAGE TITLE]";
+
+export const PAGE_DESCRIPTION = `Chat with CaseyTheCareerBot, Mary Hulbert's AI assistant.`;
+
+export const EMPTY_CITATION_MESSAGE = "Unspecified source";
